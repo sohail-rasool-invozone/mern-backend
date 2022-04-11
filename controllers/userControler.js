3eb5960778f2d657ac82cf9d200deac55a52f2b1
Sohail Rasool <sohailrasool@mac-junaid-SSE-167.local>
2022-04-11 16:09:25 +0500

    User auth functionality

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -2,6 +2,10 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
+// @desc Auth user & get token
+// @route post/api/users/login
+// @access pulblic route
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
@@ -21,4 +25,56 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+// @desc Register a new user
+// @route Post/api/users
+// @access pulblic route
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User Already Exist')
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User Data')
+  }
+})
+
+// @desc Auth user profile
+// @route get/api/users/profile
+// @access private route
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
-import { authUser } from '../controllers/userControler.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userControler.js'
+import protect from '../middleware/authMiddleWare.js'
 
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.route('/login').post(authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
